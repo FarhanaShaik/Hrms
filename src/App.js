ea9f6b8b48f4fd3ee64e2bde6a29e3e181f15c2a
FarhanaShaik <38202104+FarhanaShaik@users.noreply.github.com>
2018-04-12 15:27:29 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-        
+        <h1>hi jagan</h1>
       
         </header>
         <p className="App-intro">
