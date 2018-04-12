9af37da5607c09f7a3d06614fd86a579e2f878de
FarhanaShaik <38202104+FarhanaShaik@users.noreply.github.com>
2018-04-12 15:25:54 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,8 +8,8 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-       <h1>Welcome to React1</h1>
+        
+      
         </header>
         <p className="App-intro">
           edit src/app
